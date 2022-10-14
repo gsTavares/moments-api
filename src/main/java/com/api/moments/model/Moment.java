d6cca47399d0c70e4cc2b8916b6f7ff59538ccc5
Gustavo Tavares <gustavo.tavares24@outlook.com>
2022-10-13 22:04:36 -0300

    feat: image getter and setter

--- src/main/java/com/api/moments/model/Moment.java
+++ src/main/java/com/api/moments/model/Moment.java
@@ -72,6 +72,14 @@ public class Moment implements Serializable {
         this.description = description;
     }
 
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
     public Date getCreatedAt() {
         return createdAt;
     }
